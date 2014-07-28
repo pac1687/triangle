dd8282f67e4c3146740f0cbd00ac749a0917d3ed
Justin Pacubas <jpacubas@gmail.com>
2014-07-28 00:43:42 -0700

    Added comments and cleaned up HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var triangle = function(side1, side2, side3) {
-  var triangleType = "";
+  var triangleType;
   if (side1 >= side2 + side3 ||
       side2 >= side1 + side3 ||
       side3 >= side1 + side2) {
@@ -14,13 +14,25 @@ var triangle = function(side1, side2, side3) {
   return triangleType
 };
 
+
+
 $(document).ready(function () {
   $("form#triangle-input").submit(function(event) {
     event.preventDefault();
 
-      side1 = parseInt($("input#side1").val());
-      side2 = parseInt($("input#side2").val());
-      side3 = parseInt($("input#side3").val());
+    // Taking input and turning into integer
+    side1 = parseInt($("input#side1").val());
+    side2 = parseInt($("input#side2").val());
+    side3 = parseInt($("input#side3").val());
+    // Storing inputs and feeding to triangle function
+    var results = triangle(side1, side2, side3);
 
+    // Outputting results to page
+    $("#answer").text(results);
+
+    // For clearing inputs after submission
+    $("input#side1").val("");
+    $("input#side2").val("");
+    $("input#side3").val("");
   });
 });
